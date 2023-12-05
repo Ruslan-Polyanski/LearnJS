d0f6ee75375381ba598eafd25261e5f8959344e8
Ruslan-Palyanski <ruslapolyanski@yandex.ru>
2023-12-05 23:07:11 +0300

    feat: add task-2.9

---
+++ js_2/2.9 Операторы сравнения/task.js
@@ -0,0 +1,11 @@
+// Операторы сравнения
+
+// Каким будет результат этих выражений?
+
+5 > 4 // true
+"ананас" > "яблоко" // false
+"2" > "12" // ture
+undefined == null // true
+undefined === null // false
+null == "\n0\n" // false
+null === +"\n0\n" // false
