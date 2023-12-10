fa4bf7dccd20c29888f6a98df1bff8ebaa050236
Ruslan-Palyanski <ruslapolyanski@yandex.ru>
2023-12-10 18:57:24 +0300

    feat: add task-2.17

---
+++ js_2/2.17 Стрелочные функции, основы/task.js
@@ -0,0 +1,16 @@
+function ask(question, yes, no) {
+  if (confirm(question)) yes()
+  else no();
+}
+
+ask(
+  "Вы согласны?",
+  function() { alert("Вы согласились."); },
+  function() { alert("Вы отменили выполнение."); }
+);
+
+const ask = (question, yes, no) => {
+  confirm(question) ? yes() : no();
+}
+
+ask("Вы согласны?", () => alert("Вы согласились."), () => alert("Вы отменили выполнение."));
