74992eeacd57d1b9347109193d43f56e49547c1b
Ruslan-Palyanski <ruslapolyanski@yandex.ru>
2023-12-20 23:17:32 +0300

    feat: update task-5.1

---
+++ js_5/5.1 Методы примитивов/task.js
@@ -0,0 +1,22 @@
+let roma = {
+  name: "Рома",
+  sayHi: function() {
+    alert("Привет, дружище!");
+  }
+};
+
+roma.sayHi(); // Привет, дружище!
+
+
+
+let str = "Привет";
+
+alert( str.toUpperCase() ); // ПРИВЕТ
+
+
+alert(null.test); // ошибка
+
+
+let str1 = "Привет";
+str1.test = 5;
+console.log(str1.test);
