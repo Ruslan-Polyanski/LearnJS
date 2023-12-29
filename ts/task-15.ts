745b5b4bf48597fc80e86a4120671b44bbdbb4b6
Ruslan-Palyanski <ruslapolyanski@yandex.ru>
2023-12-29 22:02:38 +0300

    feat: add ts-15

---
+++ ts/task-15.ts
@@ -0,0 +1,7 @@
+
+// Реализуйте функцию fail(), которая выбрасывает любое исключение. 
+// Пропишете ее возвращаемый тип явно.
+
+function fail(): never {
+  throw new Error("fucntion fail is exit");
+}
