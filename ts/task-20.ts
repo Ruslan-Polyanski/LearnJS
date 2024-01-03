3c7f3297935d4cfad9c5d3042e4ba669b1229e5a
Ruslan-Palyanski <ruslapolyanski@yandex.ru>
2024-01-03 22:55:02 +0300

    feat: add task-20

---
+++ ts/task-20.ts
@@ -0,0 +1,34 @@
+
+// Реализуйте функцию last(), которая извлекает последний элемент 
+// из переданного значения. Значением может быть строка или число. 
+// Функция возвращает значение того же типа, которое было передано 
+// в качестве параметра:
+
+function isNumber(value: unknown): value is number {
+  return typeof value === 'number' && !Number.isNaN(value);
+}
+
+function isString(value: unknown): value is string {
+  return typeof value === 'string';
+}
+
+function last(value: unknown) {
+  if(isNumber(value)) {
+    const arr = value.toString().split('');
+    return +arr[arr.length - 1];
+  }
+
+  if(isString(value)) {
+    const arr = value.split('');
+    return arr[arr.length - 1];
+  }
+}
+
+
+// Передаем в качестве параметра строку
+// Функция возвращает строку
+last('hexlet'); // t
+
+// Передаем в качестве параметра число
+// Функция возвращает число
+last(12345); // 5
