8bda174dd3e7a85352d44ec8730a4adcb1704e1f
Ruslan-Palyanski <ruslapolyanski@yandex.ru>
2024-01-14 23:12:43 +0300

    feat: add ts-32

--- js_5/5.4 Массивы/learn.js
+++ js_5/5.4 Массивы/learn.js
@@ -1,3 +1,26 @@
 
 let arr1 = new Array();
 let arr2 = [];
+
+
+let fruits = [];
+fruits[123] = "Яблоко";
+
+alert( fruits.length ); // 124
+
+
+let arr = [1, 2, 3, 4, 5];
+
+arr.length = 2; // укорачиваем до двух элементов
+alert( arr ); // [1, 2]
+
+arr.length = 5; // возвращаем length как было
+alert( arr[3] ); // undefined: значения не восстановились
+
+alert( [] + 1 ); // "1"
+alert( [1] + 1 ); // "11"
+alert( [1,2] + 1 ); // "1,21"
+
+alert( [] == [] ); // false
+alert( [0] == [0] ); // false
+
