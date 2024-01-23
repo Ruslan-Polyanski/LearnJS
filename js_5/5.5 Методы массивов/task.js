d9c6caf709d30b53ffbb5e9dd6bd7cad6006f286
Ruslan-Palyanski <ruslapolyanski@yandex.ru>
2024-01-23 23:17:27 +0300

    feat: add part 3 task-5.5

--- js_5/5.5 Методы массивов/task.js
+++ js_5/5.5 Методы массивов/task.js
@@ -134,3 +134,55 @@ powerCalc.addMethod("**", (a, b) => a ** b);
 
 let result = powerCalc.calculate("2 ** 3");
 alert( result ); // 8
+
+
+// Трансформировать в массив имён
+// У вас есть массив объектов user, и в каждом из них есть user.name. 
+// Напишите код, который преобразует их в массив имён.
+
+
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+
+let users = [ vasya, petya, masha ];
+
+let names = users.reduce((acc, value) => [...acc, value.name], []);
+
+alert( names ); // Вася, Петя, Маша
+
+
+// Трансформировать в объекты
+// У вас есть массив объектов user, и у каждого из объектов есть name, surname и id.
+// Напишите код, который создаст ещё один массив объектов 
+// с параметрами id и fullName, где fullName – состоит из name и surname.
+
+let vasya1 = { name: "Вася", surname: "Пупкин", id: 1 };
+let petya1 = { name: "Петя", surname: "Иванов", id: 2 };
+let masha1 = { name: "Маша", surname: "Петрова", id: 3 };
+
+let users1 = [ vasya1, petya1, masha1 ];
+
+let usersMapped = users1.map(value => {
+  return { 
+    fullName: `${value.name} ${value.surname}`, 
+    id: value.id
+  };
+});
+
+/*
+usersMapped = [
+  { fullName: "Вася Пупкин", id: 1 },
+  { fullName: "Петя Иванов", id: 2 },
+  { fullName: "Маша Петрова", id: 3 }
+]
+*/
+console.log(usersMapped)
+alert( usersMapped[0].id ) // 1
+alert( usersMapped[0].fullName ) // Вася Пупкин
+
+
+
+
+
+
