734e084f2ab78cb04a25b0f1fc666b5f933369bd
Ruslan-Palyanski <ruslapolyanski@yandex.ru>
2024-01-30 23:39:21 +0300

    feat: add task-5.8

--- js_5/5.8 WeakMap и WeakSet/task.js
+++ js_5/5.8 WeakMap и WeakSet/task.js
@@ -23,3 +23,33 @@ let isRead = Symbol("isRead");
 messages[0][isRead] = true;
 
 
+// Хранение времени прочтения
+// Есть массив сообщений такой же, как и в предыдущем задании.
+// Теперь вопрос стоит так: какую структуру данных вы бы предложили 
+// использовать для хранения информации о том, когда сообщение было прочитано?
+// В предыдущем задании нам нужно было сохранить только факт прочтения «да или нет». 
+// Теперь же нам нужно сохранить дату, и она должна исчезнуть 
+// из памяти при удалении «сборщиком мусора» сообщения.
+
+let messages = [
+  { text: "Hello", from: "John" },
+  { text: "How goes?", from: "John" },
+  { text: "See you soon", from: "Alice" }
+];
+
+const weakMapDate = new WeakMap();
+
+const dateReadMessage = (message) => {
+  if(!weakMapDate.has(message)) {
+    weakMapDate.set(message, new Date())
+  }
+
+  return weakMapDate.get(message);
+}
+
+dateReadMessage(messages[0])
+dateReadMessage(messages[1])
+dateReadMessage(messages[0])
+
+
+
