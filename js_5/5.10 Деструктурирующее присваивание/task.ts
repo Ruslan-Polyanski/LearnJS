e63e6703a5a02750678c9d8df9086c8b5f688f07
Ruslan-Palyanski <ruslapolyanski@yandex.ru>
2024-02-02 19:00:43 +0300

    feat: add past part task-5.10

--- js_5/5.10 Деструктурирующее присваивание/task.ts
+++ js_5/5.10 Деструктурирующее присваивание/task.ts
@@ -19,3 +19,36 @@
   alert( isAdmin ); // false
 
 };
+
+
+
+() => {
+
+  type TSalaries = {
+    [key: string]: number 
+  }
+
+  type TName = null | string;
+
+  let salaries: TSalaries = {
+    "John": 100,
+    "Pete": 300,
+    "Mary": 250
+  };
+  
+  function topSalary(salaries: TSalaries): TName {
+    let name: TName = null;
+    let salary = 0;
+  
+    for(let [key, value] of Object.entries(salaries)) {
+      if(salary < value) {
+        [name, salary] = [key, value]
+      }
+    }
+  
+    return name;
+  }
+  
+  topSalary(salaries)
+
+}
