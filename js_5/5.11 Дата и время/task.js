42d838ae710978c15e183351288ccb168fff37a1
Ruslan-Palyanski <ruslapolyanski@yandex.ru>
2024-02-07 18:56:49 +0300

    feat: add task-5.11

--- js_5/5.11 Дата и время/task.js
+++ js_5/5.11 Дата и время/task.js
@@ -188,8 +188,24 @@
 
 () => {
 
+  const formatDate = (date) => {
+    const dateNow = new Date();
+    const result = (dateNow - date)/1000;
 
-  
+    if(result < 1) return "прямо сейчас";
+    if(result < 60) return `${result} сек. назад`;
+    if(result < 3660) return (result/60) + " мин. назад";
+
+    const day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
+    const month = (date.getMonth() + 1) < 10 ? '0' + (date.getMonth() + 1) : (date.getMonth() + 1);
+    const year = date.getFullYear().toString().slice(-2);
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+
+    return `${day}.${month}.${year} ${hours}:${minutes}`;
+  }
+
+
   alert( formatDate(new Date(new Date - 1)) ); // "прямо сейчас"
 
   alert( formatDate(new Date(new Date - 30 * 1000)) ); // "30 сек. назад"
@@ -198,7 +214,7 @@
   
   // вчерашняя дата вроде 31.12.2016, 20:00
   alert( formatDate(new Date(new Date - 86400 * 1000)) );
-
+S
 }
 
 
