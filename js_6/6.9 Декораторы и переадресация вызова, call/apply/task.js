b31a4e3048f439fd2f38592b82a9875f86270f9a
Ruslan-Palyanski <ruslapolyanski@yandex.ru>
2024-02-29 23:24:26 +0300

    feat: add part task-6.9

--- js_6/6.9 Декораторы и переадресация вызова, call/apply/task.js
+++ js_6/6.9 Декораторы и переадресация вызова, call/apply/task.js
@@ -66,3 +66,115 @@
   worker.myHardFunc(100)
 
 }
+
+
+
+() => {
+
+  let worker = {
+    name: 'worker',
+    slow(min, max) {
+      return min + max + ' - ' + this.name;
+    }
+  };
+
+  function hash(args) {
+    return args[0] + ',' + args[1];
+  }
+
+  function cachingDecorator(func, hash) {
+    const cachMap = new Map();
+
+    return function() {
+      const key = hash(arguments);
+      if(cachMap.has(key)) return cachMap.get(key);
+
+      const result = func.call(this, ...arguments);
+      // const result = func.apply(this, arguments);
+      cachMap.set(key, result)
+
+      return result;
+    }
+  }
+  
+
+  worker.slow = cachingDecorator(worker.slow, hash);
+  worker.slow(3, 5)
+  worker.slow(13, 7)
+
+}
+
+
+
+() => {
+
+  // перенаправлением вызова
+
+  let wrapper = function() {
+    return func.apply(this, arguments);
+  };
+
+}
+
+
+
+() => {
+
+  // заимствование метода
+
+  function hash() {
+    alert( [].join.call(arguments) ); // 1,2
+  }
+  
+  hash(1, 2);
+
+// arr.join(glue)
+// Пускай первым аргументом будет glue или, в случае отсутствия аргументов, им будет запятая ","
+// Пускай result будет пустой строкой "".
+// Добавить this[0] к result.
+// Добавить glue и this[1].
+// Добавить glue и this[2].
+// …выполнять до тех пор, пока this.length элементов не будет склеено.
+// Вернуть result.
+
+}
+
+
+
+() => {
+
+  let worker = {
+    name: 'worker',
+    slow(min, max, ...rest) {
+      if(rest.length) {
+        return min + max + ',' + rest + ' - ' + this.name;
+      }
+      return min + max + ' - ' + this.name;
+    }
+  };
+
+  function hash(args) {
+    return [].join.call(args)
+  }
+
+  function cachingDecorator(func, hash) {
+    const cachMap = new Map();
+
+    return function() {
+      const key = hash(arguments);
+      if(cachMap.has(key)) return cachMap.get(key);
+
+      const result = func.call(this, ...arguments);
+      // const result = func.apply(this, arguments);
+      cachMap.set(key, result)
+
+      return result;
+    }
+  }
+  
+
+  worker.slow = cachingDecorator(worker.slow, hash);
+  worker.slow(3, 5, 10, 12, 7, 4)
+  worker.slow(13, 7)
+
+}
