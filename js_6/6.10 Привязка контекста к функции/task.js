f578d865d495c1982062ef2b30968b803c0485ba
Ruslan-Palyanski <ruslapolyanski@yandex.ru>
2024-03-06 23:36:54 +0300

    feat: add task-6.10

--- js_6/6.10 Привязка контекста к функции/task.js
+++ js_6/6.10 Привязка контекста к функции/task.js
@@ -126,6 +126,21 @@
   // Что нужно передать в вызов функции askPassword в коде ниже, 
   // чтобы она могла вызывать функцию user.login(true) как ok и функцию user.login(false) как fail?
 
+  function askPassword(ok, fail) {
+    let password = prompt("Password?", '');
+    if (password == "rockstar") ok();
+    else fail();
+  }
+  
+  let user = {
+    name: 'John',
+  
+    login(result) {
+      alert( this.name + (result ? ' logged in' : ' failed to log in') );
+    }
+  };
+  
+  askPassword(user.login.bind(user, true), user.login.bind(user, false));
 
 }
 
