11833c1ab4e6376a053fed2f6a1240683b408d80
Ruslan-Palyanski <ruslapolyanski@yandex.ru>
2024-04-15 23:43:11 +0300

    feat: add task ts

---
+++ ts/Классы/task-35.ts
@@ -0,0 +1,34 @@
+
+() => {
+
+  // Реализуйте класс CustomFile, в конструктор которого передается имя файла и размер 
+  // в байтах или другой файл. Внутри класса определите метод toString(), 
+  // который должен вернуть форматированную строку в формате (copy) <file-name> (<size> bytes). 
+  // (copy) должно выводиться только в том случае, если файл является копией другого файла.
+
+  class CustomFile {
+
+    name: string;
+    size: number
+
+    constructor({name: string, size: number}) {
+      this.name = name;
+      this.size = size;
+    }
+
+    toString() {
+      return `${this.name} (${this.size})`;
+    }
+
+  }
+
+  const file = new CustomFile({ name: 'open-world.jpeg', size: 1000 });
+  console.log(file.toString()); // open-world.jpeg (1000 bytes)
+  
+  const file2 = new CustomFile(file);
+  console.log(file2.toString()); // (copy) open-world.jpeg (1000 bytes)
+  
+  const file3 = new CustomFile(file2);
+  console.log(file2.toString()); // (copy) open-world.jpeg (1000 bytes)
+
+}
