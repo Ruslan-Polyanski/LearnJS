1cc035cec8f0d8cc55e9c6e2d44eb7c797f9af29
Ruslan-Palyanski <ruslapolyanski@yandex.ru>
2024-05-05 22:06:42 +0300

    feat: add node server and endpoint

--- server/v1/server.js
+++ server/v1/server.js
@@ -1,35 +1,63 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
+const delay = (ms) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve()
+    }, ms)
+  })
+};
 
-let count = 0;
+const readFile = (pathToFile) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(pathToFile, (error, data) => {
+      if(error) {
+        reject(error)
+      } else {
+        resolve(data)
+      }
+    })
+  })
+};
 
-const server = http.createServer((request, response) => {
+const FAVICON = path.join(__dirname, 'public', 'favicon.ico');
+
+const server = http.createServer(async (request, response) => {
 
   switch (request.url) {
 
     case '/favicon.ico':
       response.setHeader('Content-Type', 'image/x-icon');
-      break
+      fs.createReadStream(FAVICON).pipe(response)
+      return;
 
-    case '/technikon': 
-      count++
-      response.write('mechanical engineer')
+    case '/mt': 
+    try{
+      const data = await readFile('pages/index.html')
+      response.write(data)
+    } catch (error) {
+      response.write('500')
+    } finally {
+      response.end()
+    }
+      
+    
+      
       break
 
     case '/it': 
-      count++
-      response.write('frontend')
+      await delay(3000)
+      response.write('ok in it')
+      response.end()
       break
 
     default: 
-      count++
       response.write('404')
-
+      response.end()
   }
 
-    response.write(' I count data ' + `${count}`)
-    response.end()
-  
 });
 
 server.listen(3004)
