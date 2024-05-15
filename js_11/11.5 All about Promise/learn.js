2caa06a5891badba16b95cdad6c03b181ec56f93
Ruslan-Palyanski <ruslapolyanski@yandex.ru>
2024-05-15 21:30:15 +0300

    feat: add task promise

--- js_11/11.5 All about Promise/learn.js
+++ js_11/11.5 All about Promise/learn.js
@@ -31,3 +31,113 @@
 }
 
 
+
+() => {
+
+  function myFunc(src, callBack) {
+    const script = new Promise((resolve, reject) => {
+      setTimeout(() => {resolve(src)}, 3000)
+    })
+
+    script.then((value) => {
+      callBack(value)
+    })
+  }
+
+  myFunc('myFile', (script) => {console.log(script)})
+
+}
+
+
+
+() => {
+
+  function loadScript(src, callBack) {
+    let script = document.createElement('script');
+    script.src = src;
+    script.onload = () => callBack(null, script);
+    script.onerror = () => callBack(new Error('Не удалось загрузить'))
+  }
+
+  loadScript('myScript', (error, script) => {
+    if(error) {
+      // обработка ошибки
+    } else {
+      // Обрабатываем результат
+    }
+  })
+
+}
+
+
+() => {
+
+  loadScript('script-1', (error, script) => {
+    if(error) {
+      handleError(error)
+    } else {
+      loadScript('script-2', (error, script) => {
+        if(error) {
+          handleError(error)
+        } else {
+          //...
+        }
+      }
+    }
+  })
+
+}
+
+
+() => {
+
+  loadScript('script-1', step1)
+
+  function step1(error, script) {
+    if(error) {
+      handleError(error)
+    } else {
+      loadScript('script-2', step2)
+    }
+  }
+
+  function step2(error, script) {
+    if(error) {
+      handleError(error)
+    } else {
+      loadScript('script-2', step3)
+    }
+  }
+
+}
+
+
+() => {
+
+  function loadScript(src) {
+    return new Promise(function(resolve, reject) {
+      let script = document.createElement('script');
+      script.src = src;
+  
+      script.onload = () => resolve(script);
+      script.onerror = () => reject(new Error(`Ошибка загрузки скрипта ${src}`));
+  
+      document.head.append(script);
+    });
+  }
+
+
+  let promise = loadScript("https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.js");
+
+  promise.then(
+    script => alert(`${script.src} загружен!`),
+    error => alert(`Ошибка: ${error.message}`)
+  );
+
+  promise.then(script => alert('Ещё один обработчик...'));
+
+}
+
+
+
+
